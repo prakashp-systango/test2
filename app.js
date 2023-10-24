9177540c9bb4bd3a8b69957f664026d7239ddb77
prakash-systango <ppatel@systango.com>
2023-10-24 10:36:00 +0530

    cicd

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.get("/user",(req,res)=>{
     res.send("welcome to jungle")
 })
 
+app.get("/hello",(req,res)=>{
+    res.send("welcome to babas club")
+})
 app.listen(8000,()=>{
     console.log("server is running")
 })
